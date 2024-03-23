a012d7eaf1929e619681b67158ab2e03d43fd9f2
Myo Thu <myot5400@gmail.com>
2024-03-23 15:15:50 +0630

    Photo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,7 +21,7 @@
             <div class="card mb-0">
               <div class="card-body">
                 <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                  <img src="{{asset('admin/images/logos/dark-logo.svg')}}" width="180" alt="">
+                  <img src="../assets/images/logos/dark-logo.svg" width="180" alt="">
                 </a>
                 <p class="text-center">Your Social Campaigns</p>
                 <form>
@@ -40,7 +40,7 @@
                   <a href="./index.html" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign Up</a>
                   <div class="d-flex align-items-center justify-content-center">
                     <p class="fs-4 mb-0 fw-bold">Already have an Account?</p>
-                    <a class="text-primary fw-bold ms-2" href="{{route('login')}}">Sign In</a>
+                    <a class="text-primary fw-bold ms-2" href="./authentication-login.html">Sign In</a>
                   </div>
                 </form>
               </div>
