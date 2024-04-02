ce6d90c830d26833a7e39f460c8883069ef4d667
Jaz <htetwaihlyankyaw27@gmail.com>
2024-04-02 09:20:38 +0630

    Logout Redirect

--- resources/views/admin/Model/brand_model.blade.php
+++ resources/views/admin/Model/brand_model.blade.php
@@ -46,20 +46,12 @@
                             @csrf
 
 
-<<<<<<< HEAD
-                            {{-- Model  --}}
-                            {{-- <div class="form-group mb-3">
-                                <label for="modelName" class="form-label">Brand</label>
-                                <select class="form-select @error('BrandId') is-invalid @enderror" name="BrandId" id="modelName">
-                                    <option value="">Choose Brand</option>
-=======
                             {{-- Model
                             <div class="form-group mb-3">
                                 <label for="brandName" class="form-label">Brand</label>
                                 <select class="form-select @error('BrandId') is-invalid @enderror" name="BrandId"
                                     id="brandName">
                                     <option value="" selected>Choose Brand</option>
->>>>>>> 421ae06a804c0ba25378d339caedebd0ca3caac7
                                     @foreach ($data as $brand)
                                         <option value="{{ $brand->id }}"
                                             @if (old('BrandId', $brand->id) == $brand->id) selected @endif>{{ $brand->name }}</option>
@@ -91,7 +83,7 @@
                                         {{ $message }}
                                     </div>
                                 @enderror
-                            </div> --}}
+                            </div>
 
                             <div class="form-group mb-3">
                                 <label for="modelName" class="form-label">Brand</label>
