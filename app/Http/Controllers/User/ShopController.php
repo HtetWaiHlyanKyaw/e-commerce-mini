47cb4467823d233efbf1451ed394dee389a7694d
Jaz <htetwaihlyankyaw27@gmail.com>
2024-04-08 15:45:00 +0630

    Change Filter Content

--- app/Http/Controllers/User/ShopController.php
+++ app/Http/Controllers/User/ShopController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Brand;
+use App\Models\Product;
 
 class ShopController extends Controller
 {
     public function shop(){
-        return view('user.shop');
+
+        $brands = Brand::get();
+        $minPrice = Product::min('price');
+        $maxPrice = Product::max('price');
+        $products = Product::all();
+        $uniqueColors = $products->pluck('color')->unique();
+        $uniqueStorage = $products->pluck('storage_option')->unique();
+        return view('user.shop',compact('brands','minPrice','maxPrice','uniqueColors','uniqueStorage'));
     }
 
 
